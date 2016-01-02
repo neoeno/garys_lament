0a74ccd433fcefb6592615cb6fc39b42a364b49b
Caden Lovelace <caden@herostrat.us>
2016-01-02 23:23:24 +0000

    Remove opening the browser line
    WHY do people have this

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('core-js/fn/object/assign');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-var open = require('open');
 
 new WebpackDevServer(webpack(config), config.devServer)
 .listen(config.port, 'localhost', function(err) {
@@ -11,6 +10,4 @@ new WebpackDevServer(webpack(config), config.devServer)
     console.log(err);
   }
   console.log('Listening at localhost:' + config.port);
-  console.log('Opening your system browser...');
-  open('http://localhost:' + config.port + '/webpack-dev-server/');
 });
