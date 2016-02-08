84df5942fcc621924ba538cc39550f96dc73bdf8
Caden Lovelace <caden@herostrat.us>
2016-02-08 22:38:50 +0000

    Add walking north into portal behaviour
    We want the character to walk 'over' that tile, to get a 'walking into
    the door' effect.
    
    Pretty pleased by how easy this has turned out to implement.

--- src/sequences/movementIntentSequence.js
+++ src/sequences/movementIntentSequence.js
@@ -1,11 +1,18 @@
 import * as Game from '../lib/Game';
 import * as UI from '../lib/UI';
 import movementSequence from './movementSequence';
+import * as actions from '../actions/game';
 
 export default (store) => () => {
   let state = store.getState().game;
   if (Game.isMovingBlocked(state)) { return; }
 
   let movement = UI.activeKeyToMovement(state.movementKeyPressed);
-  movementSequence(store)(movement);
+
+  if (movement.x == 0 && movement.y == 0) {
+    store.dispatch(actions.setWalkingStatus({walking: false}));
+    store.dispatch(actions.setMovingStatus({moving: false}));
+  } else {
+    movementSequence(store)(movement);
+  }
 };
--- src/sequences/movementSequence.js
+++ src/sequences/movementSequence.js
@@ -3,19 +3,29 @@ import * as Tiled from '../lib/Tiled';
 import * as actions from '../actions/game';
 import maps from '../game/maps';
 import followPortalSequence from './followPortalSequence';
+import storePromise from '../lib/storePromise';
 
-export default (store) => (movement) => {
+let move = store => targetPosition => {
+  store.dispatch(actions.setMovingStatus({moving: true}));
+  store.dispatch(actions.beginMoveTo(targetPosition));
+  return storePromise(store)({moving: false});
+};
+
+export default (store) => async (movement) => {
   let state = store.getState().game;
   let targetPosition = Game.movePosition(state)(movement);
 
   store.dispatch(actions.faceDirection(Game.faceMovementDirection(movement)));
-  store.dispatch(actions.setWalkingStatus(Game.walkingStatus(movement)));
+  store.dispatch(actions.setWalkingStatus({walking: true}));
 
-  if (Tiled.canWalkTo(targetPosition)(maps[state.map])) {
-    store.dispatch(actions.setMovingStatus(Game.movingStatus(movement)));
-    store.dispatch(actions.beginMoveTo(targetPosition));
-  } else if (Tiled.isPortalAtPosition(maps[state.map])(targetPosition)) {
+  if (Tiled.isPortalAtPosition(maps[state.map])(targetPosition)) {
+    if (Game.movementToFacing(movement) == 'north') {
+      await move(store)(targetPosition);
+    }
+
     let portal = Tiled.getPortalAtPosition(maps[state.map])(targetPosition);
     followPortalSequence(store)(portal);
+  } else if (Tiled.canWalkTo(targetPosition)(maps[state.map])) {
+    move(store)(targetPosition);
   }
 };
