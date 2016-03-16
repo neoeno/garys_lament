3c30ba0a94f564c8b7fcb12e2b7f25875944695a
Caden Lovelace <caden@herostrat.us>
2016-03-16 08:52:04 +0000

    Clone episode 2 -> episode 3

--- src/game/episodes/index.js
+++ src/game/episodes/index.js
@@ -1,4 +1,5 @@
 import flat_1 from './flat_1/index';
 import flat_2 from './flat_2/index';
+import flat_3 from './flat_3/index';
 
-export default { flat_1, flat_2 };
+export default { flat_1, flat_2, flat_3 };
