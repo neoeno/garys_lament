c4d0d1a4b60529e9e2eb05fd5ac96c10e293bde5
Caden Lovelace <caden@herostrat.us>
2016-03-26 14:18:32 +0000

    Remove sequencers
    We don't need them anymore now everything is tick-based. Again — great
    that this is coming together!

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import { Provider } from 'react-redux';
 import configureStore from './stores';
 import App from './containers/App';
 import * as UIObserver from './lib/UIObserver';
-import configureSequencer from './lib/Sequencer';
 import { tick } from './actions/game';
 
 const store = configureStore();
-const sequencer = configureSequencer(store);
-window.sequencer = sequencer;
 
-UIObserver.observe(window)(sequencer.dispatch);
-setInterval(() => sequencer.dispatch(tick()), 16);
+UIObserver.observe(window)(store.dispatch);
+setInterval(() => store.dispatch(tick()), 16);
 
 render(
   <Provider store={store}>
