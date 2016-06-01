061999d4e22046899945df0895e9fba785ddb32c
Kay Lovelace <kay@herostrat.us>
2016-06-01 21:43:37 +0100

    First attempt

---
+++ levels/level_1/default.go
@@ -0,0 +1,15 @@
+components {
+  id: "tilemap"
+  component: "/levels/level_1/lounge.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
